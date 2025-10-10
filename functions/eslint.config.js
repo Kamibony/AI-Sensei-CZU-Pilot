5476dd0fb437fbdb77e1c119e56603063824d32e
Kamibony <equilibrium.probioticos@gmail.com>
2025-10-10 04:52:36 -0300

    Update eslint.config.js

--- functions/eslint.config.js
+++ functions/eslint.config.js
@@ -1,51 +1,37 @@
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import eslintPluginImport from "eslint-plugin-import";
-
-// The recommended configs from typescript-eslint are applied to all files by default.
-// To fix this, we'll remap them to ONLY apply to our TypeScript source files.
-const scopedRecommended = tseslint.configs.recommended.map(config => ({
-  ...config,
-  files: ["src/**/*.ts"],
-}));
+import pluginJs from "@eslint/js";
 
 export default [
+  // Globálna konfigurácia ignonovania
   {
-    // Global ignores. This is the most important part.
-    // It prevents ESLint from looking inside the compiled output directory.
-    ignores: ["lib/"],
+    ignores: [
+      "lib/",
+      "node_modules/",
+    ],
   },
+  
+  // Základné pravidlá odporúčané ESLintom
+  pluginJs.configs.recommended,
+  
+  // Konfigurácia pre TypeScript súbory
+  ...tseslint.configs.recommended,
+
+  // Vlastné pravidlá a nastavenia
   {
-    // General configuration for all linted files
     languageOptions: {
       globals: {
         ...globals.browser,
         ...globals.node,
       },
-    },
-    plugins: {
-      import: eslintPluginImport,
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
     rules: {
-        "import/no-unresolved": 0, // Common issue with module resolution
-        "quotes": ["error", "double"],
-    },
-  },
-  // Now, spread the SCOPED recommended configs.
-  // These will only apply to files matching "src/**/*.ts".
-  ...scopedRecommended,
-  {
-    // Override the no-unused-vars rule to allow underscore-prefixed variables
-    files: ["src/**/*.ts"],
-    rules: {
-      "@typescript-eslint/no-unused-vars": [
-        "error",
-        {
-          "argsIgnorePattern": "^_",
-          "varsIgnorePattern": "^_",
-          "caughtErrorsIgnorePattern": "^_",
-        },
-      ],
+      "quotes": ["error", "double"],
+      "import/no-unresolved": 0,
     },
   },
 ];
