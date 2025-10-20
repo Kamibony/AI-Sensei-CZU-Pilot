748e11e1b24457a120dc90ae834258f3a35dd6bf
Kamibony <equilibrium.probioticos@gmail.com>
2025-10-19 22:47:01 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,21 @@ module.exports = {
     './public/**/*.html',
     './public/**/*.js'
   ],
+  
+  // ===== OPRAVA: Pridaná sekcia 'safelist' na zachovanie štýlov =====
+  safelist: [
+    {
+      pattern: /(bg|border|text)-(green|blue|orange)-(50|100|200|500|700|800)/,
+    },
+    {
+      pattern: /border-l-4/,
+    },
+    {
+      pattern: /before:(content|absolute|left-0|text-orange-500|font-bold)/,
+    }
+  ],
+  // =============================================================
+
   theme: {
     extend: {},
   },
