05478af4ff520598fa94bd7ad1b468623d7c7fdf
Kamibony <equilibrium.probioticos@gmail.com>
2025-11-09 08:20:20 -0300

    Update professor.js

--- public/js/professor.js
+++ public/js/professor.js
@@ -1,16 +1,12 @@
-// public/js/professor.js
-
 import './views/professor/professor-app.js';
 
 /**
  * Inicializuje hlavný komponent profesorského dashboardu.
- * Premenované na initProfessorApp pre kompatibilitu s app.js
  */
 export async function initProfessorApp(user) {
     const roleContentWrapper = document.getElementById('role-content-wrapper');
     if (!roleContentWrapper) return;
 
-    // Vložíme hlavný web komponent, ktorý si už rieši všetko sám
     roleContentWrapper.innerHTML = `<professor-app></professor-app>`;
     
     console.log("Professor app initialized for user:", user.uid);
